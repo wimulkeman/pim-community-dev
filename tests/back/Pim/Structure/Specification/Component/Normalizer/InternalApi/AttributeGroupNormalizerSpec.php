1db7f5b53f91581043064cb9b09370c3b6da0c1b
Samir Boulil <samir.boulil@akeneo.com>
2020-04-10 16:41:53 +0200

    PIM-9177: Fix update attribute group with lot of attributes

--- tests/back/Pim/Structure/Specification/Component/Normalizer/InternalApi/AttributeGroupNormalizerSpec.php
+++ tests/back/Pim/Structure/Specification/Component/Normalizer/InternalApi/AttributeGroupNormalizerSpec.php
@@ -5,7 +5,7 @@ namespace Specification\Akeneo\Pim\Structure\Component\Normalizer\InternalApi;
 use Akeneo\Pim\Structure\Component\Model\AttributeInterface;
 use Doctrine\Common\Persistence\ObjectRepository;
 use PhpSpec\ObjectBehavior;
-use Akeneo\Pim\Structure\Component\Normalizer\ExternalApi\AttributeGroupNormalizer;
+use Akeneo\Pim\Structure\Component\Normalizer\InternalAPI\AttributeGroupNormalizer;
 use Akeneo\Pim\Structure\Component\Model\AttributeGroupInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
@@ -59,31 +59,8 @@ class AttributeGroupNormalizerSpec extends ObjectBehavior
                 ],
                 'meta' => [
                     'id' => 1,
-                    'attributeCount' => 2,
-                    'totalAttributeCount' => 2,
                 ]
             ]
         );
-    }
-
-    function it_normalizes_an_attribute_group_with_a_maximum_of_attributes(
-        $stdNormalizer,
-        AttributeGroupInterface $attributeGroup,
-        ObjectRepository $attributeRepository,
-        AttributeInterface $attribute
-    ) {
-        $attribute->getCode()->willReturn('attribute');
-        $attribute->getSortOrder()->willReturn(1);
-        $attributeGroup->getId()->willReturn(1);
-
-        $data = ['code' => 'my_attribute_group', 'labels' => [], 'attributes' => array_fill(0, 501, 'attribute')];
-
-        $stdNormalizer->normalize($attributeGroup, 'standard', [])->willReturn($data);
-        $attributeRepository->findBy(['code'=> array_fill(0, 500, 'attribute')])->willReturn(array_fill(0, 500, $attribute));
-
-        $actual = $this->normalize($attributeGroup, 'internal_api', []);
-        $actual['meta']['attribute_count']->shouldBe(500);
-        $actual['meta']['total_attribute_count']->shouldBe(501);
-        $actual['attributes']->shouldHaveCount(500);
     }
 }
