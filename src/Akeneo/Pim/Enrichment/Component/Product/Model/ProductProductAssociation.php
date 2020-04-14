d755336e997f5b71b6dee3780694704fd6dd9b3b
Julien Sanchez <sumbobyboys@gmail.com>
2020-04-14 11:53:57 +0200

    MET-223: attempt to change the doctrine configuration

---
+++ src/Akeneo/Pim/Enrichment/Component/Product/Model/ProductProductAssociation.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Akeneo\Pim\Enrichment\Component\Product\Model;
+
+/**
+ * Product association entity
+ *
+ * @author    Filips Alpe <filips@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class ProductAssociation
+{
+    private $id;
+    private $productAssociation;
+    private $product;
+    private $quantity;
+}
